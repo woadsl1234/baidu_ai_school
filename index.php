7905b8804c43adcbf822829c35a9a82f3e9c1e20
apple <apple@ckj123.local>
2018-08-05 22:22:42 +0800

    之前做完了=。=忘记上传了

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<h1> ckj123 </h1>
 <?php
-phpinfo();
+// header("Content-Type: text/plain");
+
+function complexStrtolower($regex, $value) {
+    return preg_replace(
+        '/(' . $regex . ')/i',
+        'strtolower("\\1")',
+        $value
+    );
+}
+
+foreach ($_GET as $regex => $value) {
+    echo complexStrtolower($regex, $value) . "\n";
+}
