3e07789c737d045613cb456dc2729143a1787091
apple <apple@ckj123.local>
2018-08-12 15:04:39 +0800

    test

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
-<?php
-// header("Content-Type: text/plain");
-
-function complexStrtolower($regex, $value) {
-    return preg_replace(
-        '/(' . $regex . ')/i',
-        'strtolower("\\1")',
-        $value
-    );
-}
-
-foreach ($_GET as $regex => $value) {
-    echo complexStrtolower($regex, $value) . "\n";
-}
+<?php 
+   echo "<h1>ckj123</h1>";
